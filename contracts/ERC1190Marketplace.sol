9532bad5de735c797dc36fdc6c949a7d5fcc1728
Tommaso Azzalin <azzalintommaso@gmail.com>
2022-01-11 13:14:13 +0100

    Created half smart contract for managing the marketplace

---
+++ contracts/ERC1190Marketplace.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+import "./IERC1190Marketplace.sol";
+import "non-fungible-royalty-token/contracts/ERC1190Tradable.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+
+contract ERC1190Marketplace is Context, IERC1190Marketplace {
+    mapping(address => address[]) private _collectionsOwned;
+
+    mapping(address => address) private _collectionCreators;
+
+    address[] private _creators;
+    
+    address[] private _collections;
+
+    mapping(address => mapping(uint256 => address[])) private _ownershipLicenseApprovalRequests;
+
+    mapping(address => mapping(uint256 => address[])) private _creativeLicenseApprovalRequests;
+
+
+    function getCollections()
+        external
+        view
+        override
+        returns (address[] memory)
+    {
+        return _collections;
+    }
+
+    function getCollections(address collectionOwner)
+        external
+        view
+        override
+        returns (address[] memory)
+    {
+        require(
+            collectionOwner != address(0),
+            "ERC1190Marketplace: collectionOwner cannot be the zero address."
+        );
+
+        return _collectionsOwned[collectionOwner];
+    }
+
+    function deployNewCollection(
+        string calldata name,
+        string calldata symbol,
+        string calldata baseURI
+    ) external override returns (address) {
+        ERC1190Tradable newCollection = new ERC1190Tradable(
+            name,
+            symbol,
+            baseURI
+        );
+        address newCollectionAddress = address(newCollection);
+        address creator = _msgSender();
+
+        newCollection.transferOwnership(creator);
+
+        _collectionsOwned[creator].push(newCollectionAddress);
+        _collectionCreators[newCollectionAddress] = creator;
+        _creators.push(creator);
+        _collections.push(newCollectionAddress);
+
+        return newCollectionAddress;
+    }
+
+    function requireOwnershipLicenseTransferApproval(
+        address collectionAddress,
+        uint256 tokenId
+    ) external override {}
+
+    function requireCreativeLicenseTransferApproval(
+        address collectionAddress,
+        uint256 tokenId
+    ) external override {}
+
+    function approveOwnershipLicenseTransfer(
+        address collectionAddress,
+        uint256 tokenId
+    ) external override {}
+
+    function approveCreativeLicenseTransfer(
+        address collectionAddress,
+        uint256 tokenId
+    ) external override {}
+
+    function getOwnershipLicenseTransferRequests(
+        address collectionAddress,
+        uint256 tokenId
+    ) external view override returns (address[] memory) {}
+
+    function getCreativeLicenseTransferRequests(
+        address collectionAddress,
+        uint256 tokenId
+    ) external view override returns (address[] memory) {}
+}
---
+++ contracts/IERC1190Marketplace.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+interface IERC1190Marketplace {
+    function getCollections()
+        external
+        view
+        returns (address[] memory contractAddresses);
+
+    function getCollections(address collectionOwner)
+        external
+        view
+        returns (address[] memory contractAddresses);
+
+    function deployNewCollection(
+        string calldata name,
+        string calldata symbol,
+        string calldata baseURI
+    ) external returns (address contractAddress);
+
+    function requireOwnershipLicenseTransferApproval(
+        address collectionAddress,
+        uint256 tokenId
+    ) external;
+
+    function requireCreativeLicenseTransferApproval(
+        address collectionAddress,
+        uint256 tokenId
+    ) external;
+
+    function approveOwnershipLicenseTransfer(
+        address collectionAddress,
+        uint256 tokenId
+    ) external;
+
+    function approveCreativeLicenseTransfer(
+        address collectionAddress,
+        uint256 tokenId
+    ) external;
+
+    function getOwnershipLicenseTransferRequests(address collectionAddress, uint256 tokenId)
+        external view returns(address[] memory requests);
+
+    function getCreativeLicenseTransferRequests(address collectionAddress, uint256 tokenId)
+        external view returns(address[] memory requests);
+}
